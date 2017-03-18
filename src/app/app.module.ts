49fb38b27d0b0fd07db90d9497a3d099cb5ca271
mgwill03 <sekimworld03@gmail.com>
2017-03-18 10:52:38 -0600

    Played around some more and added video play function we are going to use

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { Example1Component } from './example1/example1.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { VideoDisplayComponent } from './video-display/video-display.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Example1Component
+    Example1Component,
+    NavigationBarComponent,
+    VideoDisplayComponent
   ],
   imports: [
     BrowserModule,
--- src/app/example1/example1.component.ts
+++ src/app/example1/example1.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-example1',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Example1Component implements OnInit {
 
-  constructor() { }
+  constructor(){}
 
   ngOnInit() {
   }
